ea0b6d834b1d35076a0ed5af8cc886c9b3c71420
Veydaszn <mvngone4l@gmail.com>
2025-11-14 19:22:40 +0300

    Initialize Program module with Main entry point

---
+++ Program.vb
@@ -0,0 +1,8 @@
+Module Program
+    <STAThread>
+    Sub Main()
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New frmPayroll())   ' start with Payroll form
+    End Sub
+End Module
